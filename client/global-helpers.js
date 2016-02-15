afcbbae58e0023eabfb0eb584cef26c51357c0bd
Nico Gangloff <nicolas@gangloff.cx>
2016-02-15 17:44:29 -0500

    Jump to first error

--- client/global-helpers.js
+++ client/global-helpers.js
@@ -3,8 +3,8 @@
 //});
 
 Template.registerHelper('nestedFieldOptions', function(name, kw) {
-  kw.hash.field = name.replace(/\$/, kw.hash.index);
-  return kw.hash;
+    kw.hash.field = name.replace(/\$/, kw.hash.index);
+    return kw.hash;
 });
 Template.registerHelper('numberOfFieldsToArray', function(i) {
     var times = [];
@@ -14,9 +14,15 @@ Template.registerHelper('numberOfFieldsToArray', function(i) {
     return times;
 });
 
-Template.registerHelper("logVar", function(input){
+Template.registerHelper("logVar", function(input) {
     console.log(input);
 });
-Template.registerHelper("isZero", function(index){
+Template.registerHelper("isZero", function(index) {
     return index === 0;
+});
+Template.registerHelper("jump", function(index) {
+    $('html, body').animate({
+        scrollTop: $("input[type=text]:first").offset().top
+    }, 500);
+    $("input[type=text]:first").focus();
 });
--- client/page-one.js
+++ client/page-one.js
@@ -1,23 +1,24 @@
 Template.familyForm.helpers({
-	// data: function() {
-	// 	return Session.get("data");
-	// },
-	schema: function () {
+    // data: function() {
+    // 	return Session.get("data");
+    // },
+    schema: function() {
         var mySchma = familyFormSchema;
 
         return mySchma;
-	},
-	action: function() {
-		return function (els, callbacks, changed) {
-			console.log("[forms] Action running!");
-			console.log("[forms] Form data!", this);
-			console.log("[forms] HTML elements with `.reactive-element` class!", els);
-			console.log("[forms] Callbacks!", callbacks);
-			console.log("[forms] Changed fields!", changed);
-			callbacks.success(); // Display success message.
-			// callbacks.reset();   // Run each Element's custom `reset` function to clear the form.
-			Session.set("data", this);
-			// Router.go('/expensesForm')
-		};
-	}
+    },
+    action: function() {
+        return function(els, callbacks, changed) {
+
+            // Router.go('/expensesForm')
+            console.log("[forms] Action running!");
+            console.log("[forms] Form data!", this);
+            console.log("[forms] HTML elements with `.reactive-element` class!", els);
+            console.log("[forms] Callbacks!", callbacks);
+            console.log("[forms] Changed fields!", changed);
+            callbacks.success(); // Display success message.
+            // callbacks.reset();   // Run each Element's custom `reset` function to clear the form.
+            Session.set("data", this);
+        };
+    }
 });
