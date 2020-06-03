0e89b293a3ca9d806e5e9769aa9fea62b187cfc4
Nico Gangloff <nicolas.gangloff@ynhh.org>
2020-06-03 19:16:44 -0400

    React Admin: - Using applicationType for Items description - Support for groupArray recursite display

--- client/forms_react/admin/ReactFormAdmin.js
+++ client/forms_react/admin/ReactFormAdmin.js
@@ -9,22 +9,65 @@ export const ReactFormAdmin = ({ type, id, ...props }) => {
     const application = Applications.findOne({ _id: id });
     return { application, ready: subscription.ready() };
   }, []);
-  console.log(application, id, type);
 
-  if (ready) {
+  const { applicationType, readyType } = useTracker(() => {
+    if (ready) {
+      const subscription = Meteor.subscribe("applicationType", type);
+      const applicationType = ApplicationType.findOne({ applicationType: type });
+      return { applicationType, readyType: subscription.ready() }
+    }
+    else return { }
+  }, [ready])
+
+  const generateFields = (definition, data) => {
+    ret = (
+      <div>
+        {definition
+          .filter(d => d.type !== "html")
+          .map((key,i) => {
+            return <Wrapper key={i} label={key.label}>
+              {((key) => {
+                switch (key.type) {
+                  case "groupArray":
+                    return (
+                      <ul className="list-group">
+                        {data[key.name].map((d, i) => {
+                          return (
+                            <li key={i} className="list-group-item">
+                              <div>{generateFields(key.definition, d)}</div>
+                              <div style={{clear: "both"}}></div>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    );
+                  case "file":
+                    console.log(data, key.name);
+                    
+                    return (
+                      <div>
+                        <a href={`/files/${data[key.name + "Id"]}`} target="_blank">
+                          Download
+                        </a>
+                      </div>
+                    );
+                  default:
+                    return <div>{data[key.name]}</div>;
+                }
+              })(key)}
+            </Wrapper>
+         })
+        }
+      </div>
+    );
+
+    return ret;
+  }
+
+  if (readyType) {
     return (
       <div>
-        {Object.keys(application).map((key, i) => {
-          let value = application[key].toString()
-          if (key.endsWith("Id")) value = <a href={`/files/${application[key]}`} target="_blank">Download</a>
-
-          
-          return (
-            <div key={i}>
-              <Wrapper label={key}><div>{value}</div></Wrapper>
-            </div>
-          );
-        })}
+        {generateFields(applicationType.definition, application)}
       </div>
     );
   } else {
